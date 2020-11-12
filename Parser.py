d8f586c13ec506355bf90938802745a080f8d426
YunMin-Song <thd930308@naver.com>
2020-11-13 04:01:43 +0900

    엑셀 데이터 DB로 이동

--- Parser.py
+++ Parser.py
@@ -1,6 +1,7 @@
 from openpyxl import load_workbook
 import database
 
+
 def Excel():
     wb = load_workbook('SampleData.xlsx')
 
@@ -10,12 +11,16 @@ def Excel():
 
     for r in ws.rows:  # 엑셀에서 각 행과 열로 순회하기 위함
         OrderDate = r[0].value  # 첫째 열의 값 : 그룹
-        data = r[2].value  # 둘째 열의 값 : 주소
-
+        Region = r[1].value
+        Rep = r[2].value
+        Item = r[3].value  # 둘째 열의 값 : 주소
+        Units = r[4].value
+        Unit_Cost = r[5].value
+        East_Regula = r[6].value
+        Total = r[7].value
         print(OrderDate)
-        print(data)
-        database.insert(OrderDate, data, data)
-        # print(r)
+        database.insert(OrderDate, Region, Rep, Item, Units, Unit_Cost, East_Regula, Total)
+
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -3,27 +3,57 @@ from sqlalchemy import Column, INTEGER, String, create_engine
 from sqlalchemy.orm import sessionmaker
 import Parser
 
-engine = create_engine('mysql://root:@localhost/test?charset=utf8', convert_unicode=False)
+engine = create_engine('mysql://root:@localhost/justq?charset=utf8', convert_unicode=False)
 Session = sessionmaker()
 Base = declarative_base()
 session = Session(bind=engine)
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'data'
 
-    id = Column(INTEGER, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
+    id = Column(INTEGER,primary_key=True)
+    orderdate = Column(String)
+    region = Column(String)
+    rep = Column(String)
+    item = Column(String)
+    units = Column(INTEGER)
+    unit_cost = Column(INTEGER)
+    east_regula=Column(String)
+    total=Column(INTEGER)
 
 
 def __repf__(self):
-    return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-                   self.name, self.fullname, self.nickname)
+    return "<User(" \
+           "OrderData='%s', " \
+           "Region='%s', " \
+           "Rep='%s', " \
+           "Item='%s'," \
+           "Units='%d'," \
+           "Unit_Cost='%f'" \
+           "East_Regula='%s'" \
+           "Total='%f')>" % \
+           (
+               self.OrderData,
+               self.Region,
+               self.Rep,
+               self.Item,
+               self.Units,
+               self.Unit_Cost,
+               self.East_Regula,
+               self.total
+           )
 
 
-def insert(NAME, FULLNAME, NICKNAME):
-    ed_user = User(name=NAME, fullname=FULLNAME, nickname=NICKNAME)
+def insert(ORDERDATE, REGION, REP, ITEM, UNITS, UNIT_COST, EAST_REGULA, TOTAL):
+    ed_user = User(orderdate=ORDERDATE,
+                   region=REGION,
+                   rep=REP,
+                   item=ITEM,
+                   units=UNITS,
+                   unit_cost=UNIT_COST,
+                   east_regula=EAST_REGULA,
+                   total=TOTAL
+                   )
     session.add(ed_user)
     session.commit()
